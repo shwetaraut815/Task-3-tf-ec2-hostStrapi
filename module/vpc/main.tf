8c28e29974464c9cf92f6f1b1fbde82129fae689
shwetaraut815 <shwetaraut815@gmail.com>
2026-02-03 11:14:12 +0530

    Add VPC, subnets, and route table resources

---
+++ module/vpc/main.tf
@@ -0,0 +1,48 @@
+resource "aws_vpc" "test" {
+    cidr_block = var.cidr_block
+    tags = {
+        Name = "my-vpc"
+    }
+}
+
+## internet gateway
+
+resource "aws_internet_gateway" "igw" {
+    vpc_id = aws_vpc.test.id
+}
+
+## public subnet
+
+resource "aws_subnet" "public" {
+    vpc_id = aws_vpc.test.id
+    cidr_block = var.public_cidr
+    availability_zone =  us-east-1b
+    tags = {
+        Name = "public-automation"
+    }
+}
+## private subnet
+resource "aws_subnet" "private" {
+    vpc_id = aws_vpc.test.id
+    cidr_block = var.private_cidr
+    availability_zone = us-east-1a
+    tags = {
+        Name = "private-subnet"
+    }
+}
+### route table
+resource "aws_route_table" "public-rt" {
+    vpc_id = aws_vpc.test.id
+    route {
+        cidr_block = "0.0.0.0/0"
+        gateway_id = aws_internet_gateway.igw.id
+    }
+}
+
+## subnet association
+
+resource "aws_route_table_association" "ass" {
+    subnet_id = aws_subnet.public.id
+    route_table_id = aws_route_table.public-rt.id
+  
+}
